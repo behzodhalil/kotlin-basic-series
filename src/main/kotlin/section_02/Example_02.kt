3462a8956fbc15c2c893aa1b8d32a31f66215719
Behzod Bozorboev <behzoddev@gmail.com>
2021-10-28 11:38:02 +0900

    Add Example_02.kt to [Section 02]

---
+++ src/main/kotlin/section_02/Example_02.kt
@@ -0,0 +1,11 @@
+package section_02
+
+fun main() {
+    val list = listOf(1,2,3,4,5,6)
+
+    println(list.last())
+    // Output: 6
+    println(list[3])
+    println(list.get(3))
+    // Output: 4
+}
