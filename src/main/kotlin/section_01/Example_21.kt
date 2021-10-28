6963703735654c9372422320eeffd4e7d6ba010b
Behzod Bozorboev <behzoddev@gmail.com>
2021-10-28 11:28:22 +0900

    Add Example_21.kt to [Section 01]

---
+++ src/main/kotlin/section_01/Example_21.kt
@@ -0,0 +1,16 @@
+package section_01
+
+// range:
+// A range is essentially just an interval between two values,
+// usually numbers: a start and an end. We write it using the '..' operator
+
+fun main() {
+    val oneToTen = 1..10
+    val tenToTwenty = 10..20
+    val fiveToTen = 5..10
+
+    /* for(i in oneToTen) {
+        println("$i : $oneToTen")
+    }*/
+
+}
