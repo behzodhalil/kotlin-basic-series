fb8d1ac5b0b9fac965b1d13814899fe65b293b4e
Behzod Bozorboev <behzoddev@gmail.com>
2021-10-28 11:28:41 +0900

    Add Example_24.kt to [Section 01]

---
+++ src/main/kotlin/section_01/Example_24.kt
@@ -0,0 +1,15 @@
+package section_01
+
+fun main() {
+    /*for(i in 50 downTo 1 step 5){
+        println(i)
+    }
+    for(i in 1..50 step 5) {
+        println(i)
+    }*/
+    for(i in 1 until 50) {
+        println(i)
+        // until:
+        // If the condition is from 1 to 50, iteration 1 to 49 is performed
+    }
+}
