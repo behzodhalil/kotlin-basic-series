1c88ba177e815cd5cc1791059426202b2bd57f47
Behzod Bozorboev <behzoddev@gmail.com>
2021-10-28 11:28:11 +0900

    Add Example_20.kt to [Section 01]

---
+++ src/main/kotlin/section_01/Example_20.kt
@@ -0,0 +1,21 @@
+package section_01
+
+
+fun main() {
+
+    // Declared a variable with a value of 10
+    var number = 10
+
+    // while:
+    // The body is executed while the condition is true
+
+    while(number>=5){
+        println("Number: $number")
+        number--
+    }
+    while(number<=10){
+        println("Number +: $number")
+        number++
+    }
+
+}
