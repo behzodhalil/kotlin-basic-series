732397089fab47d5924400a6322bceea54597500
Behzod Bozorboev <behzoddev@gmail.com>
2021-10-28 11:42:21 +0900

    Update Example_02.kt to [Section 02]

--- src/main/kotlin/section_02/Example_02.kt
+++ src/main/kotlin/section_02/Example_02.kt
@@ -1,7 +1,14 @@
 package section_02
 
+// An example for immutable list
 fun main() {
+    // A list is a collection of items with a specific order.
     val list = listOf(1,2,3,4,5,6)
+    val listLanguages = listOf("English","Korean","Russian","Uzbek","Polish")
+
+    for(item in listLanguages){
+        println(item)
+    }
 
     println(list.last())
     // Output: 6
