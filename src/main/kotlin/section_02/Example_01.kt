3506ee34eaec36c76be5c903d55ba72156a12034
Behzod Bozorboev <behzoddev@gmail.com>
2021-10-28 11:37:54 +0900

    Add Example_01.kt to [Section 02]

--- src/main/kotlin/section_02/Example_01.kt
+++ src/main/kotlin/section_02/Example_01.kt
@@ -1,2 +1,11 @@
 package section_02
 
+fun main() {
+    val set = hashSetOf(1,7,53)
+    val list = arrayListOf(1,8,53)
+    val map = hashMapOf(1 to "one",2 to "two",3 to "three")
+
+    println(set.javaClass)
+    println(list.javaClass)
+    println(map.javaClass)
+}
