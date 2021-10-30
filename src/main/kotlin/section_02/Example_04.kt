6d4115fcf0de7ebbaee4920d5c462cb64b682ded
Behzod Bozorboev <behzoddev@gmail.com>
2021-10-30 13:15:41 +0900

    Update Example_04.kt to [Section 02]

--- src/main/kotlin/section_02/Example_04.kt
+++ src/main/kotlin/section_02/Example_04.kt
@@ -1,2 +1,15 @@
 package section_02
 
+// In Kotlin, you don't need to create all those meaningless classes.
+// Instead, you can place functions directly at the top level of source file,
+// outside any class.
+
+fun joinToString(): String{ return "String"}
+
+// Java
+/* public final class Example_04Kt {
+   @NotNull
+   public static final String joinToString() {
+      return "String";
+   }
+}*/
