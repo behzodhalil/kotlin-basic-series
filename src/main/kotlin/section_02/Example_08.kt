fcf09593dbbd1092b53cd97da119ff9e2cb30734
Behzod Bozorboev <behzoddev@gmail.com>
2021-10-30 14:15:24 +0900

    Add Example_07.kt to [Section 02]

---
+++ src/main/kotlin/section_02/Example_08.kt
@@ -0,0 +1,14 @@
+package section_02
+import section_02.lastChar as last
+
+// 'as' keyword:
+// We can change the name of the class or function you're importing
+// using the 'as' keyword
+
+// Changing a name on import is useful when you have several functions
+// with the same in different packages and you want to use them in the same file.
+
+fun main() {
+
+    val character = "Kotlin".last()
+}
