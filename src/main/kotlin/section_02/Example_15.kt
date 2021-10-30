2560a9fd63293190ff96478b64820d7afb883cc8
Behzod Bozorboev <behzoddev@gmail.com>
2021-10-30 15:41:37 +0900

    Add Example_15.kt to [Section 02]

---
+++ src/main/kotlin/section_02/Example_15.kt
@@ -0,0 +1,7 @@
+package section_02
+
+fun main() {
+    // Pair(number, name) -> val (number,name) -> number = 1; name = one
+    val (number,name) = 1 to "one"
+    // This feature is called a destructuring declaration
+}
