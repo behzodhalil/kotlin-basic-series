0d231448177571e776c5f924d7b3397f9abbeade
Behzod Bozorboev <behzoddev@gmail.com>
2021-10-30 15:33:57 +0900

    Add Example_14.kt to [Section 02]

---
+++ src/main/kotlin/section_02/Example_14.kt
@@ -0,0 +1,7 @@
+package section_02
+
+// mapOf():
+// Returns a new read-only map with the specified contents, given as a list of pairs
+// where the first value is the key and the second is the value.
+
+val map = mapOf(1 to "one",2 to "two")
