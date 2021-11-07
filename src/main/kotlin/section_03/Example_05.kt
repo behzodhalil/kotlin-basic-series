02c4720087f4a16b7d1990ac6c6dde29d480d7c8
Behzod Bozorboev <behzoddev@gmail.com>
2021-11-07 15:36:06 +0900

    Add Example_05.kt to [Section 03]

---
+++ src/main/kotlin/section_03/Example_05.kt
@@ -0,0 +1,10 @@
+package section_03
+
+fun main() {
+    val string1 = "Kotlin"
+    val string2 = "Kotlin"
+    println(string1 == string2)
+    println(string1.equals(string2))
+    println(string1 === string2)
+
+}
