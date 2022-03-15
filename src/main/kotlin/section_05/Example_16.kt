e026b4d436e523b54d07f5602212e88265e37986
Behzod Bozorboev <behzoddev@gmail.com>
2022-03-15 11:21:55 +0900

    Add Example_16.kt to [Section 05]

---
+++ src/main/kotlin/section_05/Example_16.kt
@@ -0,0 +1,15 @@
+package section_05
+
+val numberOne = 1
+
+fun fizz() {
+    val numberTwo = 2
+    println(numberOne + numberTwo)
+}
+val buzz = {
+    val numberThree = 3
+    println(numberOne + numberThree)
+}
+fun main() {
+    // Here we can see numberOne, but not numberTwo or numberThree
+}
