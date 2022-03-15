e3de88f3f3e51acfd5415149b1240d854416380c
Behzod Bozorboev <behzoddev@gmail.com>
2022-03-15 11:21:44 +0900

    Add Example_15.kt to [Section 05]

---
+++ src/main/kotlin/section_05/Example_15.kt
@@ -0,0 +1,11 @@
+package section_05
+
+fun main() {
+    val list1: MutableList<Int> = mutableListOf()
+    var list2: List<Int> = listOf()
+
+    list1.add(1)
+    list2 = list2 + 1
+    println(list1)
+    println(list2)
+}
